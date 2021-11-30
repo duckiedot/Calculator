2b1296300f58b03b48140bea9502bb8e0ed1de1d
Duckiedot <m.malinkiewicz01@gmail.com>
2021-11-30 12:54:12 +0100

    made all calc logic

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,12 +20,12 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
-
-        private bool Inprogress
-        { get; set; }
-
+        
+        private string FirstInput { get; set; }
+        private string SecondInput { get; set; }
         public MainWindow()
         {
+            string dpa = FirstInput;
             InitializeComponent();
         }
 
@@ -38,29 +38,82 @@ namespace Calculator
         {
             var inputButton = sender as Button;
             string buttonValue = Convert.ToString(inputButton.Content);
+
             switch (buttonValue)
             {
                 case "+":
                 case "-":
                 case "/":
                 case "*":
-                    Calc(buttonValue);
+                    Operation.Text = buttonValue;
+                    break;
+                case "=":
+                    Calc(FirstInput, SecondInput, Operation.Text);
                     break;
                 default:
+                    CalcLogic(buttonValue);
+                    OutputText.Text += buttonValue;                
                     break;
-            }
-            OutputText.Text += inputButton.Content;
+            }  
         }
 
         private void Clear_all()
         {
             OutputText.Text = string.Empty;
             Operation.Text = string.Empty;
+            FirstInput = null;
+            SecondInput = null;
         }
 
-        private void Calc(string operation)
+        private void Calc(string firstInput, string secondInput, string operation)
         {
-            
+            int first = Convert.ToInt32(firstInput);
+            int second = Convert.ToInt32(secondInput);
+
+            int result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = first + second;
+                    break;
+                case "-":
+                    result = first - second;
+                    break;
+                case "*":
+                    result = first * second;
+                    break;
+                case "/":
+                    if (first == 0 || second == 0)
+                    {
+                        Clear_all();
+                        OutputText.Text = "you can't devide with 0 xd";
+                        return;
+                    }
+                    result = first / second;
+                    break;
+
+            }
+
+            OutputText.Text = Convert.ToString(result);
+            FirstInput = Convert.ToString(result);
+            SecondInput = null;
+        }
+
+        private void CalcLogic(string buttonValue)
+        {
+            if (Operation.Text == string.Empty && Operation.Text == string.Empty)
+            {
+                FirstInput += buttonValue;
+            }
+            else if (Operation.Text != string.Empty && FirstInput != null)
+            {
+                if (SecondInput == null) 
+                { 
+                    OutputText.Text = string.Empty; 
+                }
+                SecondInput += buttonValue;
+            }
         }
     }
 }
